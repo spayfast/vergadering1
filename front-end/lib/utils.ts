cf29215652923eb05dc9d59af53343070215d569
SeppeDW <69519243+spayfast@users.noreply.github.com>
2024-05-17 23:37:10 +0200

    set up completed

---
+++ front-end/lib/utils.ts
@@ -0,0 +1,8 @@
+import { type ClassValue, clsx } from "clsx"
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
